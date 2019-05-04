514cc71f24071410ccf5413ab1bd9f752b0a7acc
geowat <ldsxxs@163.com>
2019-05-04 21:55:10 +0800

    update dist

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue'
 import './plugins/element.js'
 
 Vue.config.productionTip = false
-let serverLocation = 'http://localhost'
+// let serverLocation = 'http://localhost'
+let serverLocation = 'http://45.32.135.124'
 Vue.prototype.userServer = serverLocation + ':8801'
 Vue.prototype.fundServer = serverLocation + ':8802'
 
