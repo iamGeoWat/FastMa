c55bffa1f401b231aeb13a806619fbba094f488b
geowat <ldsxxs@163.com>
2019-05-23 17:13:53 +0800

    db config

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -2,8 +2,8 @@ module.exports = {
   mysql: {
     host: '127.0.0.1',
     user: 'root',
-    // password: 'FastMa8888',
-    password: 'Liuxikai1998', // for develop use
+    password: 'FastMa8888',
+    // password: 'Liuxikai1998', // for develop use
     database: 'fastma',
     connectionLimit: 10
   }
