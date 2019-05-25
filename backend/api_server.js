51b7d2b3381466bd25ef6f0ab2411c6389e36614
geowat <ldsxxs@163.com>
2019-05-25 18:06:58 +0800

    Update api_server.js

--- backend/api_server.js
+++ backend/api_server.js
@@ -35,6 +35,7 @@ const redis = new Redis()
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS");
   next();
 });
 app.use(bodyParser.json())
