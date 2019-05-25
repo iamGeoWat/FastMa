edaa43729c8f993067cc5e15a1100c3941dce084
geowat <ldsxxs@163.com>
2019-05-25 17:27:47 +0800

    api location change

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,7 @@ import './plugins/element.js'
 
 Vue.config.productionTip = false
 // let serverLocation = 'http://localhost'
-let serverLocation = 'https://fastma.io'
+let serverLocation = 'https://45.32.135.124'
 // Vue.prototype.userServer = serverLocation + ':8801'
 // Vue.prototype.fundServer = serverLocation + ':8801'
 // Vue.prototype.gameServer = serverLocation + ':8801'
