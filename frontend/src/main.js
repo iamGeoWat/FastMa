c9f0b16463556cdb4cf5996b3869903e29a7542b
geowat <ldsxxs@163.com>
2019-05-25 17:48:11 +0800

    frontend changes

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,7 @@ import './plugins/element.js'
 
 Vue.config.productionTip = false
 // let serverLocation = 'http://localhost'
-let serverLocation = 'https://45.32.135.124'
+let serverLocation = 'https://fastma.io'
 // Vue.prototype.userServer = serverLocation + ':8801'
 // Vue.prototype.fundServer = serverLocation + ':8801'
 // Vue.prototype.gameServer = serverLocation + ':8801'
